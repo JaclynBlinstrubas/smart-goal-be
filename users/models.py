85a3fd7e58a48e1b446ab85d7113275e0c5b1e36
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-06-30 14:03:14 -0400

    removed user avatar

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField( verbose_name='email', max_length=255, unique=True)
-    avatar = models.CharField(max_length=500)
     REQUIRED_FIELDS = ['username']
     USERNAME_FIELD = 'email'
 
