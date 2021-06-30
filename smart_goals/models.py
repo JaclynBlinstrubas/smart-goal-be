a95c459c4ff86a9808274ccbf43afa55bbb806ea
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-06-30 13:56:18 -0400

    migrated models

--- smart_goals/models.py
+++ smart_goals/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=250)
+    goal = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Goal(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="goals")
+    specific = models.CharField(max_length=500)
+    measureable = models.CharField(max_length=500)
+    attainable = models.CharField(max_length=500)
+    relevent = models.CharField(max_length=500)
+    timely = models.CharField(max_length=500)
+    overall_goal = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.author
+
+class Notes(models.Model):
+    body = models.TextField()
+    
