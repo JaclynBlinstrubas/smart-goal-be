cb2da8bc2f5b82b3b17ed9e5dcf746ec968e83f6
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-07-12 15:38:43 -0400

    final changes'

--- smart_goals/models.py
+++ smart_goals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Goal(models.Model):
     specific = models.CharField(max_length=500)
     measureable = models.CharField(max_length=500)
@@ -7,7 +8,7 @@ class Goal(models.Model):
     timely = models.CharField(max_length=500)
     overall_goal = models.CharField(max_length=500)
     notes = models.TextField(blank=True)
-    owner = models.ForeignKey('users.User', related_name="owned_goals", on_delete=models.CASCADE, null = True, blank = True)
+    owner = models.ForeignKey('users.User', related_name="goals", on_delete=models.CASCADE, null = True)
 
     def __str__(self):
         return self.overall_goal
